358987492a624361302c446e8c1c5d9891888d9c
Vadim <vadik.ya@gmail.com>
2023-03-31 01:46:18 +0300

    serve static

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket::fs::FileServer;
+
+use std::env;
+
+#[get("/hello")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+#[allow(unused_must_use)]
+async fn main() {
+    dotenv::dotenv().ok();
+    rocket::build()
+        .mount("/", FileServer::from(env::var("STATIC_FILES_DIR").unwrap() + "/"))
+        .mount("/", routes![hello])
+        .launch().await;
+
 }
