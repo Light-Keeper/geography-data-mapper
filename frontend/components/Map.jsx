6fce3d5f755fb4f81e63455e8bb8b7ca9130870f
Vadim <vadik.ya@gmail.com>
2023-04-01 19:04:24 +0300

    flag

--- frontend/components/Map.jsx
+++ frontend/components/Map.jsx
@@ -1,13 +1,32 @@
 import styled from 'styled-components'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import { useEffect } from "react";
+import L from 'leaflet'
+
+const PointsLayer = () => {
+  const map = useMap();
+  useEffect(() => {
+    let markerOptions = { icon: L.icon({
+        iconUrl: '/icons/flag.svg',
+        iconSize: [20, 20],
+      }),
+
+    };
+
+    L.marker([49.026638, 31.482904], markerOptions).addTo(map);
+  }, [map]);
+
+  return null;
+}
 
 const Map = styled(({ className }) => {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom className={className}>
+    <MapContainer center={[49.026638, 31.482904]} zoom={6} scrollWheelZoom className={className}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
         url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
       />
+      <PointsLayer />
     </MapContainer>
   )
 })`
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,6 +3,7 @@ import 'leaflet/dist/leaflet.css'
 
 import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+
 import { useEffect } from 'react'
 import { FocusStyleManager } from '@blueprintjs/core'
 
