ab567acdcc9faa3c8b159dd5bc89af60210bb12d
Vadim <vadik.ya@gmail.com>
2023-04-01 23:35:37 +0300

    callout

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,17 @@
 import { AppLayout, Sidebar, MainContent } from '../components/Containers'
 import { MapWithNoSSR } from '../components/MapWithNoSSR'
+import { Callout, Intent } from "@blueprintjs/core";
 
 function HomePage() {
   return (
     <AppLayout>
-      <Sidebar />
+      <Sidebar>
+        <Callout title="Welcome to the Map" icon="info-sign" intent={Intent.PRIMARY}>
+          <p>
+            Experience the joy of discovering 100 random locations across Ukraine.
+          </p>
+        </Callout>
+      </Sidebar>
       <MainContent>
         <MapWithNoSSR />
       </MainContent>
