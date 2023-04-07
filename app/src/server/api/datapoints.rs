96853c27390fe107c77682420292ee768ba05efe
Vadim <vadik.ya@gmail.com>
2023-04-07 14:13:39 +0300

    tmp

--- app/src/server/api/datapoints.rs
+++ app/src/server/api/datapoints.rs
@@ -3,61 +3,50 @@ use rocket::serde::json::Json;
 use rocket::State;
 use serde_json::value::RawValue;
 use crate::server::dto::{Datapoint, Page};
+use rocket::form::Form;
+use rusqlite::params;
+use serde::de::Error;
 
-fn construct_order_by_sql(order: &Vec<String>) -> String {
-    let mut order_by_clauses: Vec<String> = Vec::new();
-
-    for o in order {
-        let split_param: Vec<&str> = o.split(':').collect();
-
-        if split_param.len() != 2 {
-            continue;
-        }
-
-        let column = split_param[0];
-        let direction = split_param[1].to_uppercase();
-
-        order_by_clauses.push(format!(
-            "(SELECT a.value FROM attributes a WHERE a.datapoint_id = d.id AND a.name = '{}') {}",
-            column, direction
-        ));
-    }
-
-    if !order_by_clauses.is_empty() {
-        format!("ORDER BY {}", order_by_clauses.join(", "))
-    } else {
-        String::new()
-    }
+#[derive(Debug, FromForm)]
+pub struct QueryProcessor<'a> {
+    pub offset: Option<usize>,
+    pub limit: Option<usize>,
+    pub order_by: Option<&'a str>,
+    pub from_lng: Option<f32>,
+    pub to_lng: Option<f32>,
+    pub from_lat: Option<f32>,
+    pub to_lat: Option<f32>,
 }
 
+#[get("/datapoints?<dataset>&<qparams..>")]
+pub fn datapoints(dataset: u32, qparams: QueryProcessor, db: &State<DbPool>) -> Json<Page<Datapoint>> {
+    //language=SQLite
+    let query = r#"
+        SELECT
+            d.lat,
+            d.lng,
+            x'7B' || GROUP_CONCAT( json_quote(a.name) || ':' || json_quote(a.value), ',' ) || x'7D'
+        FROM attributes a
+             JOIN datapoints d ON d.id = a.datapoint_id
+             JOIN (
+                SELECT datapoint_id, value
+                FROM attributes
+                WHERE dataset_id = ?1 AND name = ?2
+            ) ord ON ord.datapoint_id = d.id
+        WHERE a.dataset_id = ?1
+        GROUP BY d.id, ord.value
+        ORDER BY ord.value DESC
+        LIMIT ?3
+        OFFSET ?4
+        "#;
 
-#[get("/datapoints?<dataset>&<limit>&<offset>&<order_by>")]
-pub fn datapoints(
-    dataset: u32,
-    limit: Option<u32>,
-    offset: Option<u32>,
-    order_by: Vec<String>,
-    db: &State<DbPool>) -> Json<Page<Datapoint>> {
+    let offset = qparams.offset.unwrap_or(0);
+    let limit = qparams.limit.unwrap_or(100);
+    let order_by = qparams.order_by.unwrap();
 
-    let limit = limit.unwrap_or(100);
-    let offset = offset.unwrap_or(0);
-    let order_by_clause = construct_order_by_sql(&order_by);
-
-    let conn = db.get().unwrap();
-
-    let mut statemet = conn.prepare(format!(r#"
-            SELECT d.lat, d.lng, x'7B' || GROUP_CONCAT( json_quote(a.name) || ':' || json_quote(a.value), ',' ) || x'7D'
-            FROM datapoints d
-                JOIN attributes a on a.datapoint_id = d.id
-            WHERE d.dataset_id = ?1
-            GROUP BY d.id
-            {order_by_clause}
-            limit {limit}
-            offset {offset}
-        "#).as_str()).unwrap();
-
-    dbg!(&statemet);
-    let rows = statemet.query([dataset]).unwrap();
+    let connection = db.get().unwrap();
+    let mut stmt = connection.prepare(query).unwrap();
+    let mut rows = stmt.query(params![dataset,order_by,limit,offset]).unwrap();
 
     let vec1 = rows
         .mapped(|r| Ok(Datapoint {
